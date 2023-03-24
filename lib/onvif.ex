6767236a13201eac344848c12bd8929f2f58405c
Austin Hammer <austin@getsavi.com>
2023-03-24 14:31:24 -0500

    fix hex docs

---
+++ lib/onvif.ex
@@ -0,0 +1,8 @@
+defmodule Onvif do
+  @moduledoc """
+  Interface for making requests to an Onvif compatible device.
+
+  Currently supports WS Discovery probing, a subset of Device wsdl functions
+  and a subset of Media wsdl functions.
+  """
+end
