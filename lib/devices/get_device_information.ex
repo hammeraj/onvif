d72da1bed31650ddece317819e183102a4510395
Logeshwaran <9105503+waranlogesh@users.noreply.github.com>
2023-09-13 02:03:08 +0530

    Use device struct in device service API (#20)

--- lib/devices/get_device_information.ex
+++ lib/devices/get_device_information.ex
@@ -2,9 +2,13 @@ defmodule Onvif.Devices.GetDeviceInformation do
   import SweetXml
   import XmlBuilder
 
+  alias Onvif.Device
+
   def soap_action, do: "http://www.onvif.org/ver10/device/wsdl/GetDeviceInformation"
 
-  def request(uri, auth \\ :xml_auth), do: Onvif.Devices.request(uri, auth, __MODULE__)
+  @spec request(Device.t(), :basic_auth | :digest_auth | :no_auth | :xml_auth) ::
+          {:ok, any} | {:error, map()}
+  def request(device, auth \\ :xml_auth), do: Onvif.Devices.request(device, auth, __MODULE__)
 
   def request_body do
     element(:"s:Body", [element(:"tds:GetDeviceInformation")])
