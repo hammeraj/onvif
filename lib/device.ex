12949ce26cda93a4f7f8c1add27bb3bd42f6c2cf
Logeshwaran Murugesan <9105503+waranlogesh@users.noreply.github.com>
2023-09-13 01:17:08 +0530

    Add device struct

---
+++ lib/device.ex
@@ -0,0 +1,20 @@
+defmodule Onvif.Device do
+  @enforce_keys [:username, :password, :address]
+
+  defstruct @enforce_keys ++
+              [
+                :auth_type,
+                :scopes,
+                :manufacturer,
+                :model,
+                :firmware_version,
+                :serial_number,
+                :hardware_id,
+                :ntp,
+                :media_service_path,
+                time_diff_from_system_secs: 0,
+                port: 80,
+                supports_media2?: false,
+                device_service_path: "/onvif/device_service"
+              ]
+end
