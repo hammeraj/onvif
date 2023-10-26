dc4f6e4d43e82d096ba76f00c4d9cac180bfa475
Logeshwaran Murugesan <9105503+waranlogesh@users.noreply.github.com>
2023-10-26 19:18:53 +0530

    Fix the double wrapping mess

--- lib/media/ver10/get_profile.ex
+++ lib/media/ver10/get_profile.ex
@@ -15,18 +15,15 @@ defmodule Onvif.Media.Ver10.GetProfile do
   end
 
   def response(xml_response_body) do
-    res =
-      xml_response_body
-      |> parse(namespace_conformant: true, quiet: true)
-      |> xpath(
-        ~x"//s:Envelope/s:Body/tr2:GetProfilesResponse/tr2:Profiles"el
-        |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
-        |> add_namespace("trt", "http://www.onvif.org/ver10/media/wsdl")
-        |> add_namespace("tt", "http://www.onvif.org/ver10/schema")
-      )
-      |> Onvif.Media.Ver10.Profile.parse()
-      |> Onvif.Media.Ver10.Profile.to_struct()
-
-    {:ok, res}
+    xml_response_body
+    |> parse(namespace_conformant: true, quiet: true)
+    |> xpath(
+      ~x"//s:Envelope/s:Body/tr2:GetProfilesResponse/tr2:Profiles"el
+      |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
+      |> add_namespace("trt", "http://www.onvif.org/ver10/media/wsdl")
+      |> add_namespace("tt", "http://www.onvif.org/ver10/schema")
+    )
+    |> Onvif.Media.Ver10.Profile.parse()
+    |> Onvif.Media.Ver10.Profile.to_struct()
   end
 end
