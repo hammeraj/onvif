050db0faaaf2714cf41c55d204a023fb6f32dc81
Logeshwaran <9105503+waranlogesh@users.noreply.github.com>
2024-06-26 21:02:21 +0530

    Add GetServiceCapabilities for media ver10 (#70)

---
+++ lib/media/ver10/get_service_capabilities.ex
@@ -0,0 +1,44 @@
+defmodule Onvif.Media.Ver10.GetServiceCapabilities do
+  import SweetXml
+  import XmlBuilder
+
+  require Logger
+
+  alias Onvif.Device
+
+  def soap_action, do: "http://www.onvif.org/ver10/media/wsdl/GetServiceCapabilities"
+
+  @spec request(Device.t()) :: {:ok, any} | {:error, map()}
+  def request(device),
+    do: Onvif.Media.Ver10.Media.request(device, [], __MODULE__)
+
+  def request_body do
+    element(:"s:Body", [element(:"trt:GetServiceCapabilities")])
+  end
+
+  def response(xml_response_body) do
+    parsed =
+      xml_response_body
+      |> parse(namespace_conformant: true, quiet: true)
+      |> xpath(
+        ~x"//s:Envelope/s:Body/trt:GetServiceCapabilitiesResponse/trt:Capabilities"e
+        |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
+        |> add_namespace("trt", "http://www.onvif.org/ver10/media/wsdl")
+        |> add_namespace("tt", "http://www.onvif.org/ver10/schema")
+      )
+      |> Onvif.Media.Ver10.ServiceCapabilities.parse()
+      |> Onvif.Media.Ver10.ServiceCapabilities.to_struct()
+
+    response =
+      case parsed do
+        {:ok, sevice} ->
+          sevice
+
+        {:error, changeset} ->
+          Logger.error("Discarding invalid service capability: #{inspect(changeset)}")
+          %Onvif.Media.Ver10.ServiceCapabilities{}
+      end
+
+    {:ok, response}
+  end
+end
---
+++ test/media/get_service_capabilities_test.exs
@@ -0,0 +1,39 @@
+defmodule Onvif.Media.Ver10.GetServiceCapabilitiesTest do
+  use ExUnit.Case, async: true
+
+  @moduletag capture_log: true
+
+  describe "GetServiceCapabilities/1" do
+    test "should parse with correct values and defaults for non existing attributes" do
+      xml_response = File.read!("test/media/ver10/fixtures/get_service_capabilities_response.xml")
+
+      device = Onvif.Factory.device()
+
+      Mimic.expect(Tesla, :request, fn _client, _opts ->
+        {:ok, %{status: 200, body: xml_response}}
+      end)
+
+      {:ok, service_capabilities} = Onvif.Media.Ver10.GetServiceCapabilities.request(device)
+
+      assert service_capabilities == %Onvif.Media.Ver10.ServiceCapabilities{
+               exi_compression: false,
+               osd: true,
+               profile_capabilities: %Onvif.Media.Ver10.ServiceCapabilities.ProfileCapabilities{
+                 maximum_number_of_profiles: 24
+               },
+               rotation: false,
+               snapshot_uri: true,
+               streaming_capabilities:
+                 %Onvif.Media.Ver10.ServiceCapabilities.StreamingCapabilities{
+                   no_rtsp_streaming: false,
+                   non_aggregated_control: false,
+                   rtp_rtsp_tcp: true,
+                   rtp_tcp: false,
+                   rtsp_multicast: false
+                 },
+               temporary_osd_text: false,
+               video_source_mode: false
+             }
+    end
+  end
+end
