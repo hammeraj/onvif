352e9b39008ab3deec7b38c956885a8a9a802341
Paolo Oliveira <paolocmo@gmail.com>
2024-07-08 21:59:59 +0100

    #10471 Create SetSystemDateAndTime

---
+++ lib/devices/set_system_date_and_time.ex
@@ -0,0 +1,59 @@
+defmodule Onvif.Devices.SetSystemDateAndTime do
+  import SweetXml
+  import XmlBuilder
+
+  alias Onvif.Device
+  alias Onvif.Devices.SystemDateAndTime
+
+  def soap_action, do: "http://www.onvif.org/ver10/device/wsdl/SetSystemDateAndTime"
+
+  @spec request(Device.t(), list) :: {:ok, any} | {:error, map()}
+  def request(device, args) do
+    Onvif.Devices.request(device, args, __MODULE__)
+  end
+
+  def request_body(%SystemDateAndTime{} = system_date_time) do
+    element(:"s:Body", [
+      element(:"tds:SetSystemDateAndTime", [
+        element(:"tds:DateAndTime", system_date_time.date_time_type ),
+        element(:"tds:DaylightSavings", system_date_time.daylight_savings ),
+        element(
+          :"tds:TimeZone",
+          [
+            element(:"tt:TZ", system_date_time.time_zone.tz )
+          ]
+        ),
+
+        element(
+          :"tds:UTCDateTime",
+          [
+            element(:"tt:Time", [
+              element(:"tt:Hour", system_date_time.utc_date_time.time.hour ),
+              element(:"tt:Minute", system_date_time.utc_date_time.time.minute ),
+              element(:"tt:Second", system_date_time.utc_date_time.time.second )
+            ]),
+            element(:"tt:Date", [
+              element(:"tt:Year", system_date_time.utc_date_time.date.year ),
+              element(:"tt:Month", system_date_time.utc_date_time.date.month ),
+              element(:"tt:Day", system_date_time.utc_date_time.date.day )
+            ])
+          ]
+        )
+
+      ])
+    ])
+  end
+
+  def response(xml_response_body) do
+    res =
+      xml_response_body
+      |> parse(namespace_conformant: true, quiet: true)
+      |> xpath(
+        ~x"//s:Envelope/s:Body/tds:SetSystemDateAndTimeResponse/text()"s
+        |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
+        |> add_namespace("tds", "http://www.onvif.org/ver10/device/wsdl")
+        |> add_namespace("tt", "http://www.onvif.org/ver10/schema")
+      )
+    {:ok, res}
+  end
+end
