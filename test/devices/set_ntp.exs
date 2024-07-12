9ccc3791b2d4198e393c81a86549d8e251f297ab
Paolo Oliveira <paolocmo@gmail.com>
2024-07-13 00:30:30 +0100

    #10532 add test for set_ntp

---
+++ test/devices/set_ntp.exs
@@ -0,0 +1,29 @@
+defmodule Onvif.Devices.SetNTPTest do
+  use ExUnit.Case, async: true
+
+  @moduletag capture_log: true
+
+  describe "SetNTP/1" do
+    test "should parse with correct values" do
+      xml_response = File.read!("test/devices/fixtures/set_ntp_request.xml")
+
+      device = Onvif.Factory.device()
+
+      Mimic.expect(Tesla, :request, fn _client, _opts ->
+        {:ok, %{status: 200, body: xml_response}}
+      end)
+
+      {:ok, service_capabilities} = Onvif.Devices.SetNTP.request(device)
+
+      assert service_capabilities == %Onvif.Devices.NTP{
+        ntp_from_dhcp: false,
+        ntp_manual: [
+          %Onvif.Devices.NTPManual{
+            type: "IPv4",
+            ipv4_address: "6.6.6.0"
+          }
+        ]
+      }
+    end
+  end
+end
