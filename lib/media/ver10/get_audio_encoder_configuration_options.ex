e7e01c33786c3b29b1ae79573a92a6af5f6bfd41
Paolo Oliveira <paolocmo@gmail.com>
2024-08-21 10:06:27 -0300

    explicit nil is now optional

--- lib/media/ver10/get_audio_encoder_configuration_options.ex
+++ lib/media/ver10/get_audio_encoder_configuration_options.ex
@@ -13,25 +13,27 @@ defmodule Onvif.Media.Ver10.GetAudioEncoderConfigurationOptions do
   def request(device, args),
     do: Onvif.Media.Ver10.Media.request(device, args, __MODULE__)
 
-  def request_body(configuration_token, profile_token) do
+  def request_body() do
+    element(:"s:Body", [
+      element(:"trt:GetAudioEncoderConfigurationOptions")
+    ])
+  end
+
+  def request_body(configuration_token) do
     element(:"s:Body", [
       element(:"trt:GetAudioEncoderConfigurationOptions", [
-        gen_config_token_element(configuration_token),
-        gen_profile_token_element(profile_token)
+        element(:"trt:ConfigurationToken", configuration_token)
       ])
     ])
   end
 
-  def gen_profile_token_element(nil), do: []
-
-  def gen_profile_token_element(profile_token) do
-    element(:"trt:ProfileToken", profile_token)
-  end
-
-  def gen_config_token_element(nil), do: []
-
-  def gen_config_token_element(configuration_token) do
-    element(:"trt:ConfigurationToken", configuration_token)
+  def request_body(configuration_token, profile_token) do
+    element(:"s:Body", [
+      element(:"trt:GetAudioEncoderConfigurationOptions", [
+        element(:"trt:ConfigurationToken", configuration_token),
+        element(:"trt:ProfileToken", profile_token)
+      ])
+    ])
   end
 
   @spec response(any) :: {:error, Ecto.Changeset.t()} | {:ok, struct()}
