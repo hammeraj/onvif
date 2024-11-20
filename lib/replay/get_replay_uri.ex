7488bf9eb4d54c0794bfca1c3c2f12b26da8e993
Paolo Oliveira <paolocmo@gmail.com>
2024-11-20 10:32:15 -0300

    update uri

--- lib/replay/get_replay_uri.ex
+++ lib/replay/get_replay_uri.ex
@@ -11,13 +11,13 @@ defmodule Onvif.Replay.GetReplayUri do
     Onvif.Replay.request(device, args, __MODULE__)
   end
 
-  def request_body(token) do
+  def request_body(token, ss_stream \\ "RTP-Unicast", ss_protocol \\ "UDP") do
     element(:"s:Body", [
       element(:"trp:GetReplayUri", [
         element(:"trp:StreamSetup", [
-          element(:"tt:Stream", "RTP-Unicast"),
+          element(:"tt:Stream", ss_stream),
           element(:"tt:Transport", [
-            element(:"tt:Protocol", "UDP")
+            element(:"tt:Protocol", ss_protocol)
           ])
         ]),
         element(:"trp:RecordingToken", token)
@@ -26,16 +26,16 @@ defmodule Onvif.Replay.GetReplayUri do
   end
 
   def response(xml_response_body) do
-    doc = parse(xml_response_body, namespace_conformant: true, quiet: true)
     parsed_result =
-      xpath(
-        doc,
+      xml_response_body
+      |> parse(namespace_conformant: true, quiet: true)
+      |> xpath(
         ~x"//trp:Uri/text()"s
         |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
         |> add_namespace("trp", "http://www.onvif.org/ver10/replay/wsdl")
       )
 
-    IO.puts parsed_result
+    {:ok, parsed_result}
   end
 
 end
