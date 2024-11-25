4d67a05abde0871c953ff1f1244c150bff21a5c9
Paolo Oliveira <paolocmo@gmail.com>
2024-11-25 18:44:36 -0300

    GetRecordingJobs part 2

--- test/recording/get_recording_jobs_test.exs
+++ test/recording/get_recording_jobs_test.exs
@@ -17,5 +17,19 @@ defmodule Onvif.Recording.GetRecordingJobsTest do
 
       assert hd(response).job_token == "SD_DISK_20241120_211729_9C896594"
     end
+
+    test "empty recording job" do
+      xml_response = File.read!("test/recording/fixture/get_recording_jobs_empty.xml")
+
+      device = Onvif.Factory.device()
+
+      Mimic.expect(Tesla, :request, fn _client, _opts ->
+        {:ok, %{status: 200, body: xml_response}}
+      end)
+
+      {:ok, response} = Onvif.Recording.GetRecordingJobs.request(device)
+
+      assert response == []
+    end
   end
 end
