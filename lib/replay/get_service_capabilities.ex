5f3ce50c6f02512ea1ba45e24855b3856c31feeb
Paolo Oliveira <paolocmo@gmail.com>
2024-11-25 20:57:45 -0300

    Replay.GetServiceCapabilities

--- lib/replay/get_service_capabilities.ex
+++ lib/replay/get_service_capabilities.ex
@@ -3,8 +3,6 @@ defmodule Onvif.Replay.GetServiceCapabilities do
   import XmlBuilder
   require Logger
 
-  alias Onvif.Device
-
   def soap_action, do: "http://www.onvif.org/ver10/replay/wsdl/GetServiceCapabilities"
 
   def request(device) do
@@ -25,13 +23,21 @@ defmodule Onvif.Replay.GetServiceCapabilities do
         doc,
         ~x"//s:Envelope/s:Body/trp:GetServiceCapabilitiesResponse/trp:Capabilities"
         |> add_namespace("s", "http://www.w3.org/2003/05/soap-envelope")
-        |> add_namespace("trp", "http://www.onvif.org/ver10/replay/wsdl"),
+        |> add_namespace("trp", "http://www.onvif.org/ver10/replay/wsdl")
+        |> add_namespace("tt", "http://www.onvif.org/ver10/schema"),
         rtp_rtsp_tcp: ~x"//@RTP_RTSP_TCP"so,
         reverse_playback: ~x"//@ReversePlayback"so,
         session_timeout_range: ~x"//@SessionTimeoutRange"so,
-        rtsp_web_socket_uri: ~x"//@RTSPWebSocketUri"so
+        rtsp_web_socket_uri: ~x"//@RTSPWebSocketUri"so,
+        receive_source: ~x"//tt:CapabilitiesExtension/RecordingCapabilities/@ReceiverSource"so,
+        media_profile_source:
+          ~x"//tt:CapabilitiesExtension/RecordingCapabilities/@MediaProfileSource"so,
+        dynamic_recordings:
+          ~x"//tt:CapabilitiesExtension/RecordingCapabilities/@DynamicRecordings"so,
+        dynamic_tracks: ~x"//tt:CapabilitiesExtension/RecordingCapabilities/@DynamicTracks"so,
+        max_string_length: ~x"//tt:CapabilitiesExtension/RecordingCapabilities/@MaxStringLength"so
       )
 
-    {:ok, Map.merge(%Onvif.Replay.ServiceCapabilities{}, parsed_result)}
+    {:ok, Onvif.Replay.ServiceCapabilities.from_parsed(parsed_result)}
   end
 end
--- lib/replay/service_capabilities.ex
+++ lib/replay/service_capabilities.ex
@@ -1,4 +1,45 @@
 defmodule Onvif.Replay.ServiceCapabilities do
-  @derive Jason.Encoder
-  defstruct [:rtp_rtsp_tcp, :reverse_playback, :session_timeout_range, :rtsp_web_socket_uri]
+  @fields [
+    rtp_rtsp_tcp: false,
+    reverse_playback: false,
+    session_timeout_range: "0",
+    rtsp_web_socket_uri: false,
+    receive_source: false,
+    media_profile_source: false,
+    dynamic_recordings: false,
+    dynamic_tracks: false,
+    max_string_length: "0"
+  ]
+
+  defstruct Keyword.keys(@fields)
+
+  @type t() :: %__MODULE__{
+          rtp_rtsp_tcp: boolean(),
+          reverse_playback: boolean(),
+          session_timeout_range: String.t(),
+          rtsp_web_socket_uri: boolean(),
+          receive_source: boolean(),
+          media_profile_source: boolean(),
+          dynamic_recordings: boolean(),
+          dynamic_tracks: boolean(),
+          max_string_length: integer()
+        }
+
+  @doc """
+  Converts a parsed map into a %Onvif.Replay.ServiceCapabilities{} struct with validated types.
+  """
+  def from_parsed(parsed) do
+    # Ensure only valid keys and convert values
+    converted =
+      @fields
+      |> Enum.map(fn {key, _default} -> {key, convert_value(key, Map.get(parsed, key))} end)
+      |> Enum.into(%{})
+
+    struct(__MODULE__, converted)
+  end
+
+  defp convert_value(_key, "true"), do: true
+  defp convert_value(_key, "false"), do: false
+  defp convert_value(_key, nil), do: nil
+  defp convert_value(_key, value), do: value
 end
