af8c056641193a29bf9898e4ad45cf556da65eea
Paolo Oliveira <paolocmo@gmail.com>
2024-12-02 18:20:59 -0300

    Onvif.Recording.CreateRecordingJob

---
+++ test/recording/create_recording_job_test.exs
@@ -0,0 +1,21 @@
+defmodule Onvif.Recording.CreateRecordingJobTest do
+  use ExUnit.Case, async: true
+
+  @moduletag capture_log: true
+
+  describe "CreateRecordingJob/2" do
+    test "create a recording" do
+      xml_response = File.read!("test/recording/fixture/create_recording_job_success.xml")
+
+      device = Onvif.Factory.device()
+
+      Mimic.expect(Tesla, :request, fn _client, _opts ->
+        {:ok, %{status: 200, body: xml_response}}
+      end)
+
+      {:ok, response} = Onvif.Recording.CreateRecordingJob.request(device, ["SD_DISK_20241120_211729_9C896594", "9", "Active"])
+
+      assert response == "SD_DISK_20241120_211729_9C896594"
+    end
+  end
+end
