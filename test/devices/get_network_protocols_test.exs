a28e2f67fde19917f180349070c673ef200c55fb
Billal Ghilas <84322223+gBillal@users.noreply.github.com>
2025-01-16 20:02:51 +0100

    Allow providing only profile token in get video encoder configuration (#104)

--- test/devices/get_network_protocols_test.exs
+++ test/devices/get_network_protocols_test.exs
@@ -25,7 +25,9 @@ defmodule Onvif.Devices.GetNetworkProtocolsTest do
     end
 
     test "should return error when response is invalid" do
-      xml_response = File.read!("test/devices/fixtures/invalid_get_network_protocols_response.xml")
+      xml_response =
+        File.read!("test/devices/fixtures/invalid_get_network_protocols_response.xml")
+
       device = Onvif.Factory.device()
 
       Mimic.expect(Tesla, :request, fn _client, _opts ->
