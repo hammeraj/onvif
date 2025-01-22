804dfc48d9bf6f870d6ea2bd7be0f472a2d4f1fe
Austin Hammer <hammer.aj@gmail.com>
2025-01-22 16:58:54 -0600

    update to elixir 1.14 (#109)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Onvif.MixProject do
     [
       app: :onvif,
       version: "0.5.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
